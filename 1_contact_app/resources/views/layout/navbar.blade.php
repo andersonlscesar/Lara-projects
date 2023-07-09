c206d8915ad4ad5471e18196d24ee58b1f44e648
andersonlscesar <andersonlscesar@gmail.com>
2023-07-09 18:27:50 -0300

    Login finalizado

--- 1_contact_app/resources/views/layout/navbar.blade.php
+++ 1_contact_app/resources/views/layout/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-      <a class="navbar-brand text-uppercase" href="index.html">            
+      <a class="navbar-brand text-uppercase" href="{{ route('welcome') }}">            
           <strong>Contact</strong> App
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,22 +9,33 @@
           
       <!-- /.navbar-header -->
       <div class="collapse navbar-collapse" id="navbar-toggler">
-        <ul class="navbar-nav">
-          <li class="nav-item"><a href="#" class="nav-link">Companies</a></li>
-          <li class="nav-item active"><a href="{{ route('contacts.index') }}" class="nav-link">Contacts</a></li>
-        </ul>
+        @auth 
+          <ul class="navbar-nav">
+            <li class="nav-item"><a href="#" class="nav-link">Companies</a></li>
+            <li class="nav-item active"><a href="{{ route('contacts.index') }}" class="nav-link">Contacts</a></li>
+          </ul>
+        @endauth
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item mr-2"><a href="#" class="btn btn-outline-secondary">Login</a></li>
-          <li class="nav-item"><a href="#" class="btn btn-outline-primary">Register</a></li>
+          @auth 
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              John Doe
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="profile.html">Settings</a>
-              <a class="dropdown-item" href="#">Logout</a>
-            </div>
-          </li>
+                {{ Auth::user()->name }}
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="profile.html">Settings</a>
+                <form action="{{ route('logout') }}" method="POST">
+                  @csrf 
+                  @method("POST")
+                  <button class="dropdown-item btn" type="submit">Logout</button>
+                </form>
+              </div>
+            </li>
+            
+            @else
+            
+            <li class="nav-item mr-2"><a href="{{ route('login') }}" class="btn btn-outline-secondary">Login</a></li>
+            <li class="nav-item"><a href="{{ route('register') }}" class="btn btn-outline-primary">Register</a></li>
+          @endauth
         </ul>
       </div>
     </div>
--- 1_contact_app/routes/web.php
+++ 1_contact_app/routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\ContactController;
 
 Route::get('/', WelcomeController::class)->name('welcome');
-Route::delete('/contacts/{contact}/restore', [ContactController::class, 'restore'])->name('contacts.restore')->withTrashed();
-Route::delete('/contacts/{contact}/force-delete', [ContactController::class, 'forceDelete'])->name('contacts.force-delete')->withTrashed();
-Route::resource('/contacts', ContactController::class);
+
+Route::middleware('auth')->group(function() {
+
+    Route::delete('/contacts/{contact}/restore', [ContactController::class, 'restore'])->name('contacts.restore')->withTrashed();
+    Route::delete('/contacts/{contact}/force-delete', [ContactController::class, 'forceDelete'])->name('contacts.force-delete')->withTrashed();
+    Route::resource('/contacts', ContactController::class);
+
+});
